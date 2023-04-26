4bf16d31e2ae02b839d0a7fed96f959b010cc35d
Supicha Tunpoon <deadeaspcha@gmail.com>
2023-04-26 06:22:44 +0000

    Update 2 files

---
+++ components/Card.js
@@ -0,0 +1,30 @@
+function Card() {
+  const links = [
+    { card: "card1", icon: "http://placehold.co/162x325" },
+    {
+      card: "card2",
+      icon: "http://placehold.co/162x325",
+    },
+    {
+      card: "card3",
+      icon: "http://placehold.co/162x325",
+    },
+  ];
+  
+return (
+    <Card className="text-left Card">
+      <ul className="flex justify-between px-4 my-4 py-1">
+        {links.map((link) => (
+          <li key={link.id} className="flex px-2 py-1">
+            <a className="text-blue-500	no-underline text-sm" href={link.href}>
+              <img src={link.icon} />
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Nav from "../components/nav";
 import Profile from "../components/Profile";
 import Search from "../components/Search";
 import Category from "../components/Category";
+import Card from "../components/Card";
 import Head from "next/head";
 
 function Homepage() {

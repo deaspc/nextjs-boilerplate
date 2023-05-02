e1320c163931ce1439dce9b3cace7a32b6f5eae6
Supicha Tunpoon <deadeaspcha@gmail.com>
2023-05-02 09:09:11 +0000

    Update Card.js and globals.css

--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,6 @@ function Card({ title, imageURL = "https://placehold.co/162x235" }) {
         <img src={imageURL} className="w-full h-auto rounded-tl-mr-20" />
         <div class="description">
           <h3>{title}</h3>
-          <div className="card-desctiption"></div>
         </div>
       </div>
     </div>

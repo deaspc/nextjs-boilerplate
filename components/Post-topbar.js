48f4e8d792490ca89a4dfad743553170d3dde187
Supicha Tunpoon <deadeaspcha@gmail.com>
2023-05-02 15:49:48 +0000

    Update 5 files

---
+++ components/Post-topbar.js
@@ -0,0 +1,11 @@
+function Post-topbar() {
+    return (
+        <div className="topbar">
+            <div className="icon">
+                <img url="/img/">
+            </div>
+        </div>
+    );
+}
+
+export default Post-topbar;
--- pages/Post.js
+++ pages/Post.js
@@ -1,10 +1,8 @@
-import Nav from "../components/nav.js";
 import Addimg from "../components/Addimg.js";
 
 function Post() {
   return (
     <div>
-      <Nav />
       <div class="addimg-row">
         <a>
           <Addimg imageURL="/img/add.svg" title="Add image" cover="(cover)" />

fcae3bd4a709cd747bdfb03913e4e1228e3ddf0b
Supicha Tunpoon <deadeaspcha@gmail.com>
2023-05-02 13:08:46 +0000

    Update Post.js

--- pages/Post.js
+++ pages/Post.js
@@ -1,10 +1,10 @@
-Import Fill_image from "../components/Fill_image";
+import Fill_image from "../components/Fill_image";
 import Nav from "../components/nav.js";
 function Contact() {
   return (
     <div>
       <Nav />
-      <div className="text-red-600">contact</div>
+      <Fill_image>
     </div>
   );
 }

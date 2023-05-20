7eabd525edc0033a2995fc7d7110b083bd5f7abb
Supicha Tunpoon <122430994+deaspc@users.noreply.github.com>
2023-05-20 20:16:18 +0000

    Update 2 files

---
+++ pages/Notification.js
@@ -0,0 +1,15 @@
+import Nav from "../components/nav.js";
+import Notitopbar from "../components/Notitopbar.js";
+import Messagebox from "../components/Mesagebox.js";
+
+function Notification() {
+  return (
+    <div>
+      <Nav />
+      <Notitopbar />
+      <Messagebox />
+    </div>
+  );
+}
+
+export default Notification;

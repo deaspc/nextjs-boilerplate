4f298ef55ac2a8a1c1d06546c98598595319606f
Supicha Tunpoon <122430994+deaspc@users.noreply.github.com>
2023-05-20 20:13:35 +0000

    Update 6 files

---
+++ components/Messagebox.js
@@ -0,0 +1,17 @@
+function Messagebox() {
+  return (
+    <div className="messagebox">
+      <div className="userpic">
+        <img src="/img/profile2.svg" />
+      </div>
+      <div className="message-text">
+        <div className="username">ddiscute</div>
+        <div className="message">
+          Jake day1 ver.O <img src="/img/swap.svg" /> Niki day1 ver.O{" "}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Messagebox;
--- pages/Noti.js
+++ pages/Noti.js
@@ -1,11 +1,13 @@
 import Nav from "../components/nav";
 import Notitopbar from "../components/Notitopbar";
+import Messagebox from "../components/Mesagebox";
 
 function Noti() {
   return (
     <div>
       <Nav />
       <Notitopbar />
+      <Messagebox />
     </div>
   );
 }

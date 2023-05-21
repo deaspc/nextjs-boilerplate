0f15222ebcb73cd72a3413273f80349a17122ec8
Supicha Tunpoon <122430994+deaspc@users.noreply.github.com>
2023-05-21 16:08:34 +0000

    Update Profile.js and globals.css

--- components/Profile.js
+++ components/Profile.js
@@ -1,14 +1,19 @@
 function Profile() {
   return (
-      <div className="profile">
-        <div className="profile-pic">
-          <img src="/img/profile.svg" />
-        </div>
-        <div className="info-text">
+    <div className="profile">
+      <div className="profile-pic">
+        <img src="/img/profile.svg" />
+      </div>
+      <div className="info-text">
+        <div className="greeting">
           <div className="hi">HI !</div>
           <div className="name">deadea</div>
         </div>
       </div>
+      <div className="kerdar-icon">
+        <img src="/img/kerdaricon.svg" />
+      </div>
+    </div>
   );
 }
 

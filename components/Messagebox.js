12be25a1e6f93492c8ab2a2088c49398b763e57d
Supicha Tunpoon <122430994+deaspc@users.noreply.github.com>
2023-05-21 11:32:24 +0000

    Update 4 files

--- components/Messagebox.js
+++ components/Messagebox.js
@@ -1,29 +1,16 @@
-function Messagebox({
-  imageURL3 = "https://placehold.co/52x52",
-  username,
-  message1,
-  imageURL4 = "/img/swap.svg",
-  message2,
-}) {
+function Messagebox() {
   return (
-    <div className="message_wrapper">
-      <div className="message-box">
-        <div classname="message-profile">
-          <img src={imageURL3} className="w-45 h-45" />
-          
-        </div>
-        <div className="message-details">
+    <div className="message-box">
+      <div className="message-details">
+        <img src="/img/profile2.svg" />
+        <div className="swap-details">
           <div className="username">
-            <h3>{username}</h3>
+            ddiscute · 2 mins ago <img src="/img/circle.svg" />
           </div>
-          <div className="swap-details">
-            <div className="message1">
-              <h3>{message1}</h3>
-            </div>
-            <img src={imageURL4} className="w-5 h-5" />
-            <div className="message2">
-              <h3>{message2}</h3>
-            </div>
+          <div className="swap-card">
+            Jake day1 ver.O
+            <img src="/img/swap.svg" />
+            Niki day1 ver.O
           </div>
         </div>
       </div>
--- pages/Notification.js
+++ pages/Notification.js
@@ -7,18 +7,7 @@ function Notification() {
     <div>
       <Nav />
       <Notitopbar />
-      <div className="message_wrapper">
-        {/* <a href="/Post"> */}
-        <a>
-          <Messagebox
-            imageURL3="/img/profile2.svg"
-            username="ddiscute · 2 mins ago"
-            message1="Jake day1 ver.O"
-            imageURL4="/img/swap.svg"
-            message2="Niki day1 ver.O"
-          />
-        </a>
-      </div>
+      <Messagebox />
     </div>
   );
 }

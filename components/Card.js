32d6e644e3d4a2b73184919e9fa542a8910ef788
Supicha Tunpoon <122430994+deaspc@users.noreply.github.com>
2023-05-21 13:19:50 +0000

    Update Card.js and globals.css

--- components/Card.js
+++ components/Card.js
@@ -1,8 +1,10 @@
-function Card({ title, imageURL = "https://placehold.co/152x225" }) {
+function Card({ title, imageURL = "https://placehold.co/162x235" }) {
   return (
     <div className="card_wrapper">
       <div className="card-box">
-        <img src={imageURL} className="w-full h-auto rounded-tl-mr-10" />
+        <div className="card-image">
+          <img src={imageURL} className="w-full h-auto rounded-tl-mr-10" />
+        </div>
         <div className="description">
           <h3>{title}</h3>
         </div>
